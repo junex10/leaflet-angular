e12f6b42cc8ae61e619396c0e51662d1e2b8c998
Jose Herrada <joaealejunior@gmail.com>
2022-01-03 16:19:09 -0400

    Agregado robot condensed

--- src/app/shared/commons/swal.common.ts
+++ src/app/shared/commons/swal.common.ts
@@ -3,7 +3,7 @@ import { SweetAlertOptions } from 'sweetalert2';
 export const swalErrorLocation = (
     title: string = 'Error de ubicación',
     text: string = 'No se pudo obtener la ubicación actual, verifique su conexión o permita a la aplicación conocer su ubicación',
-    icon: string = '<i class="fas fa-map-marker-alt"></i>',
+    icon: string = '<i class="fas fa-map-marker-alt iconBorder"></i>',
     html: string = `
     <div class="d-flex justify-content-center align-items-center mb-3">
         <div class="mr-4 iconSwal">${icon}</div><hr>
